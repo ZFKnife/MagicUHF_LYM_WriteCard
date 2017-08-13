d0d20c83a1cb325c050f649767e3ac64d22a5303
zhangfan <13653562450@163.com>
2017-08-13 15:31:03 +0800

    从新覆盖上次信息

--- src/com/fxb/writecard/presenter/Presenter.java
+++ src/com/fxb/writecard/presenter/Presenter.java
@@ -105,7 +105,7 @@ public abstract class Presenter {
     private void writeMore(String sb) throws UnsupportedEncodingException {
         String str = sb;
         while (str.length() < 32) {
-            str += "00000000000000000000000000000000";
+            str += "0000000000000000000000000000000000000000000000000000000000000";
         }
         byte[] date = str.getBytes("UTF-8");
         int nSA = 0;
